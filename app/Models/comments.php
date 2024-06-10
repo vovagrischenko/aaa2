2cd922414e3b93746dff4503c29e81c6fa6b8a82
vovagrischenko <vova_grishchienko@mail.ru>
2024-06-10 10:13:30 +0300

    Add files via upload

--- app/Models/comments.php
+++ app/Models/comments.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\users;
+use App\Models\video;
+use App\Models\article;
 
 class comments extends Model
 {
@@ -13,14 +15,27 @@ class comments extends Model
     protected $guarded = [];
 
     static public function validate($request){
+        $result = '';
         $data = $request->post();
         $user = users::query()->select()->where('id', $request->post('user_id'))->get();
         if (!count($user)) {
-            return 'invalid user_id';
+            $result = 'invalid user_id';
         }
         if (!(array_key_exists('comment', $data)) || strlen($data['comment']) > 255 ) {
-            return 'invalid comment';
+            $result = 'invalid comment';
         }
-        return '';
+        if (!array_key_exists('target_model', $data)) {
+            try {
+                $model = sprintf('App\Models\%s',$data['target_model']);
+                $tagret = $model::find($data['target_id']);
+                if (!count($target)) {
+                    $result = 'cannot find target item';
+                }
+            } catch(Exception) {
+                $result = 'invalid target_model or target_id';
+            }
+        }
+
+        return $result;
     }
 }
